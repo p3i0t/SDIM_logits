fcce36e71e2845fe4e07408c26b6c03b39605a2c
wangxin0716 <1075837750@qq.com>
2019-12-19 18:43:30 +0800

    Initial commit.

--- sdim.py
+++ sdim.py
@@ -76,8 +76,8 @@ class SDIM(torch.nn.Module):
     def __init__(self, disc_classifier, rep_size=64, n_classes=10, mi_units=64, margin=10):
         super().__init__()
         self.disc_classifier = disc_classifier #.half()  # Use half-precision for saving memory and time.
-        self.disc_classifier.requires_grad_(requires_grad=False)  # shut down grad on pre-trained classifier.
-        self.disc_classifier.eval()  # set to eval mode.
+        # self.disc_classifier.requires_grad_(requires_grad=False)  # shut down grad on pre-trained classifier.
+        # self.disc_classifier.eval()  # set to eval mode.
 
         self.rep_size = rep_size
         self.n_classes = n_classes
