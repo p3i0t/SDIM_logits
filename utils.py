4f9e41a098b8fd69ac5af87808f58c92c7680783
wangxin0716 <1075837750@qq.com>
2019-12-31 22:02:56 +0800

    Add OOD detection evaluation.

--- utils.py
+++ utils.py
@@ -59,12 +59,12 @@ def get_dataset(data_name='cifar10', data_dir='data', train=True, label_id=None,
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
 
         transform_3d = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
 
         if train:
