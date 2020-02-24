20bb1970df934fe1cd8445c92977d6d14b039c74
王昕 <wangxin@RisingSun.lan>
2020-02-24 11:05:22 +0800

    refactor sdim_train.py

--- sdim_train.py
+++ sdim_train.py
@@ -3,20 +3,22 @@ import argparse
 import os
 import sys
 import time
+import hydra
+from omegaconf import DictConfig
+import logging
 
 import numpy as np
-
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from torchvision.utils.tensorboard import SummaryWriter
 
-#from models import ResNet18, ResNeXt
 from models import resnet18, resnet34, resnet50
 from sdim import SDIM
 from utils import cal_parameters, get_dataset, AverageMeter
 
 
-# name_dict = {'resnet': 'ResNet', 'resnext': 'ResNeXt'}
+logger = logging.getLogger(__name__)
 
 
 def get_model(name='resnet18', n_classes=10):
@@ -29,36 +31,55 @@ def get_model(name='resnet18', n_classes=10):
 
 
 def load_pretrained_model(hps):
+    """ load pretrained base discriminative classifier."""
     classifier = get_model(name=hps.classifier_name, n_classes=hps.n_classes).to(hps.device)
-
     save_name = '{}.pth'.format(hps.classifier_name)
     base_dir = 'logs/base/{}'.format(hps.problem)
     classifier.load_state_dict(torch.load(os.path.join(base_dir, save_name)))
     return classifier
 
-    # # Init model, criterion, and optimizer
-    # if hps.classifier_name == 'resnext':
-    #     classifier = ResNeXt(hps.cardinality, hps.depth, hps.n_classes, hps.base_width, hps.widen_factor).to(hps.device)
-    # elif hps.classifier_name == 'resnet':
-    #     classifier = ResNet18(n_classes=hps.n_classes).to(hps.device)
-    # else:
-    #     print('Classifier {} not available.'.format(hps.classifier_name))
 
-    # print('# Classifier parameters: ', cal_parameters(classifier))
+def run_epoch(sdim, data_loader, args, optimizer=None):
+    """
+    Run one epoch.
+    :param sdim: torch.nn.Module representing the sdim.
+    :param data_loader: dataloader
+    :param args:
+    :param optimizer: if None, then inference; if optimizer given, training and optimizing.
+    :return: mean of loss, mean of accuracy of this epoch.
+    """
+    if optimizer:
+        sdim.train()
+    else:
+        sdim.eval()
 
-    # if hps.classifier_name == 'resnext':
-    #     save_name = 'ResNeXt{}_{}x{}d.pth'.format(hps.depth, hps.cardinality, hps.base_width)
-    # elif hps.classifier_name == 'resnet':
-    #     save_name = 'ResNet18.pth'
+    loss_meter = AverageMeter('Loss')
+    MI_meter = AverageMeter('MI')
+    nll_meter = AverageMeter('NLL')
+    margin_meter = AverageMeter('Margin')
+    acc_meter = AverageMeter('Acc')
 
+    for batch_idx, (x, y) in enumerate(data_loader):
+        x, y = x.to(args.device), y.to(args.device)
+        loss, mi_loss, nll_loss, ll_margin = sdim.eval_losses(x, y)
+        loss_meter.update(loss.item(), x.size(0))
 
-    # checkpoint = torch.load(os.path.join(hps.working_dir, save_name), map_location=lambda storage, loc: storage)
-    # print('Load pre-trained checkpoint: {}'.format(save_name))
+        MI_meter.update(mi_loss.item(), x.size(0))
+        nll_meter.update(nll_loss.item(), x.size(0))
+        margin_meter.update(ll_margin.item(), x.size(0))
+            
+        if optimizer:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        with torch.no_grad():
+            preds = sdim(x).argmax(dim=1)
+        acc = (preds == y).float().mean()
+        acc_meter.update(acc.item(), x.size(0))
 
-    # classifier.load_state_dict(checkpoint['model_state'])
-    # return classifier
-
-
+    return loss_meter.avg, MI_meter.avg, nll_meter.avg, margin_meter.avg, acc_meter.avg
+  
 def train(sdim, optimizer, hps):
     sdim.disc_classifier.requires_grad = False
     torch.manual_seed(hps.seed)
@@ -70,95 +91,112 @@ def train(sdim, optimizer, hps):
     dataset = get_dataset(data_name=hps.problem, train=False, crop_flip=False)
     test_loader = DataLoader(dataset=dataset, batch_size=hps.n_batch_test, shuffle=False)
 
-    results_dict = dict({'train_loss': [], 'train_MI': [], 'train_nll': [], 'train_margin': [],
-                         'test_loss': [], 'test_MI': [], 'test_nll': [], 'test_margin': []})
+    # results_dict = dict({'train_loss': [], 'train_MI': [], 'train_nll': [], 'train_margin': [],
+    #                      'test_loss': [], 'test_MI': [], 'test_nll': [], 'test_margin': []})
+
+    # specify log dir 
+    writer = SummaryWriter('runs/sdim_train_{}_experiment'.format(args.dataset))
 
     min_loss = 1e3
-    Timer = AverageMeter('timer')
-
     for epoch in range(1, hps.epochs + 1):
-        sdim.train()
+        # train epoch
+        train_loss, mi, nll, margin, train_acc = run_epoch(sdim, train_loader, args, optimizer=optimizer)
+        logger.info('Epoch: {}, training loss: {:.4f}, mi: {:.4f}.'.format(epoch, train_loss, mi))
+        logger.info('nll: {:.4f}, margin: {:.4f}, acc: {:.4f}.'.format(nll, margin, train_acc))
+        # sdim.train()
+        # loss_meter = AverageMeter('loss')
+        # MI_meter = AverageMeter('MI')
+        # nll_meter = AverageMeter('NLL')
+        # margin_meter = AverageMeter('margin')
 
-        loss_meter = AverageMeter('loss')
-        MI_meter = AverageMeter('MI')
-        nll_meter = AverageMeter('NLL')
-        margin_meter = AverageMeter('margin')
+        # for batch_id, (x, y) in enumerate(train_loader):
+        #     x = x.to(hps.device)
+        #     y = y.to(hps.device)
 
-        end = time.time()
-        for batch_id, (x, y) in enumerate(train_loader):
-            x = x.to(hps.device)
-            y = y.to(hps.device)
+        #     optimizer.zero_grad()
 
-            optimizer.zero_grad()
+        #     loss, mi_loss, nll_loss, ll_margin = sdim.eval_losses(x, y)
+        #     loss.backward()
+        #     optimizer.step()
 
-            loss, mi_loss, nll_loss, ll_margin = sdim.eval_losses(x, y)
-            loss.backward()
-            optimizer.step()
+        #     loss_meter.update(loss.item(), x.size(0))
+        #     MI_meter.update(mi_loss.item(), x.size(0))
+        #     nll_meter.update(nll_loss.item(), x.size(0))
+        #     margin_meter.update(ll_margin.item(), x.size(0))
 
-            loss_meter.update(loss.item(), x.size(0))
-            MI_meter.update(mi_loss.item(), x.size(0))
-            nll_meter.update(nll_loss.item(), x.size(0))
-            margin_meter.update(ll_margin.item(), x.size(0))
+        # Timer.update(time.time() - end)
 
-        Timer.update(time.time() - end)
+        # logger.info('Epoch: {}, loss:{:.4f}, MI:{:.4f}, NLL:{:.4f}, margin:{:.4f}'.format(
+        #     loss_meter.avg, MI_meter.avg, nll_meter.avg, margin_meter.avg))
 
-        print('===> Epoch: {}'.format(epoch))
-        print('loss:{:.4f}, MI:{:.4f}, NLL:{:.4f}, margin:{:.4f}'.format(
-            loss_meter.avg, MI_meter.avg, nll_meter.avg, margin_meter.avg))
+        writer.add_scalar('training_loss', train_loss, epoch)
+        writer.add_scalar('training_mi', mi, epoch)
+        writer.add_scalar('training_nll', nll, epoch)
+        writer.add_scalar('training_margin', margin, epoch)
+        writer.add_scalar('training_acc', train_acc, epoch)
 
-        results_dict['train_loss'].append(loss_meter)
-        results_dict['train_MI'].append(MI_meter)
-        results_dict['train_nll'].append(nll_meter)
-        results_dict['train_margin'].append(margin_meter)
 
-        if loss_meter.avg < min_loss:
-            min_loss = loss_meter.avg
+        # results_dict['train_loss'].append(loss_meter)
+        # results_dict['train_MI'].append(MI_meter)
+        # results_dict['train_nll'].append(nll_meter)
+        # results_dict['train_margin'].append(margin_meter)
+
+        # test epoch
+        test_loss, mi, nll, margin, test_acc = run_epoch(sdim, test_loader, args)
+        logger.info('Testing loss: {:.4f}, mi: {:.4f}.'.format(test_loss, mi))
+        logger.info('nll: {:.4f}, margin: {:.4f}, acc: {:.4f}.'.format(nll, margin, test_acc))
+
+        writer.add_scalar('testing_loss', test_loss, epoch)
+        writer.add_scalar('testing_mi', mi, epoch)
+        writer.add_scalar('testing_nll', nll, epoch)
+        writer.add_scalar('testing_margin', margin, epoch)
+        writer.add_scalar('testing_acc', test_acc, epoch)
+
+        # checkpoint
+        if train_loss < min_loss:
+            min_loss = train_loss
             state = sdim.state_dict()
+            sdim_dir = 'logs/sdim/{}'.format(hps.problem)
 
-        # Evaluate accuracy on test set.
-        sdim.eval()
-        loss_meter = AverageMeter('loss')
-        MI_meter = AverageMeter('MI')
-        nll_meter = AverageMeter('NLL')
-        margin_meter = AverageMeter('margin')
+            if not os.path.exists(sdim_dir):
+                os.mkdir(sdim_dir)
 
-        acc_meter = AverageMeter('Acc')
-        for batch_id, (x, y) in enumerate(test_loader):
-            x = x.to(hps.device)
-            y = y.to(hps.device)
+            save_name = 'SDIM_{}.pth'.format(hps.classifier_name)
 
-            with torch.no_grad():
-                preds = sdim(x).argmax(dim=1)
-            acc = (preds == y).float().mean()
-            acc_meter.update(acc.item(), x.size(0))
+            checkpoint_path = os.path.join(sdim_dir, save_name)
+            torch.save(state, checkpoint_path)
 
-            with torch.no_grad():
-                loss, mi_loss, nll_loss, ll_margin = sdim.eval_losses(x, y)
+        # # Evaluate accuracy on test set.
+        # sdim.eval()
+        # loss_meter = AverageMeter('loss')
+        # MI_meter = AverageMeter('MI')
+        # nll_meter = AverageMeter('NLL')
+        # margin_meter = AverageMeter('margin')
 
-            loss_meter.update(loss.item(), x.size(0))
-            MI_meter.update(mi_loss.item(), x.size(0))
-            nll_meter.update(nll_loss.item(), x.size(0))
-            margin_meter.update(ll_margin.item(), x.size(0))
+        # acc_meter = AverageMeter('Acc')
+        # for batch_id, (x, y) in enumerate(test_loader):
+        #     x = x.to(hps.device)
+        #     y = y.to(hps.device)
 
-        print('Test accuracy: {:.3f}'.format(acc_meter.avg))
+        #     with torch.no_grad():
+        #         preds = sdim(x).argmax(dim=1)
+        #     acc = (preds == y).float().mean()
+        #     acc_meter.update(acc.item(), x.size(0))
 
-        results_dict['test_loss'].append(loss_meter)
-        results_dict['test_MI'].append(MI_meter)
-        results_dict['test_nll'].append(nll_meter)
-        results_dict['test_margin'].append(margin_meter)
+        #     with torch.no_grad():
+        #         loss, mi_loss, nll_loss, ll_margin = sdim.eval_losses(x, y)
 
-    sdim_dir = 'logs/sdim/{}'.format(hps.problem)
-    if not os.path.exists(sdim_dir):
-        os.mkdir(sdim_dir)
+        #     loss_meter.update(loss.item(), x.size(0))
+        #     MI_meter.update(mi_loss.item(), x.size(0))
+        #     nll_meter.update(nll_loss.item(), x.size(0))
+        #     margin_meter.update(ll_margin.item(), x.size(0))
 
-    save_name = 'SDIM_{}.pth'.format(hps.classifier_name)
+        # print('Test accuracy: {:.3f}'.format(acc_meter.avg))
 
-    checkpoint_path = os.path.join(sdim_dir, save_name)
-    checkpoint = {'results': results_dict, 'model_state': state}
-
-    torch.save(checkpoint, checkpoint_path)
-    print("Training time, total: {:.3f}s, epoch: {:.3f}s".format(Timer.sum, Timer.avg))
-
+        # results_dict['test_loss'].append(loss_meter)
+        # results_dict['test_MI'].append(MI_meter)
+        # results_dict['test_nll'].append(nll_meter)
+        # results_dict['test_margin'].append(margin_meter)
 
 def inference(sdim, hps):
     sdim.eval()
@@ -227,19 +265,13 @@ if __name__ == '__main__':
     parser.add_argument("--epochs", type=int, default=20,
                         help="Total number of training epochs")
 
-    # ResNeXt hyperparameters
-    parser.add_argument('--depth', type=int, default=29, help='Model depth.')
-    parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
-    parser.add_argument('--base_width', type=int, default=64, help='Number of channels in each group.')
-    parser.add_argument('--widen_factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
-
     # sdim hyperparams:
-    parser.add_argument("--image_size", type=int,
-                        default=32, help="Image size")
     parser.add_argument("--mi_units", type=int,
-                        default=64, help="output size of 1x1 conv network for mutual information estimation")
+                        default=64, help="output size of 1x1 conv network for mutual information estimation.")
     parser.add_argument("--rep_size", type=int,
-                        default=10, help="size of the global representation from encoder")
+                        default=10, help="size of the global representation from encoder.")
+    parser.add_argument("--margin", type=float,
+                        default=5, help="likelihood margin.")
     parser.add_argument("--classifier_name", type=str, default='resnet18',
                         help="classifier name: resnet18, resnet34, resnet50")
 
@@ -261,11 +293,11 @@ if __name__ == '__main__':
                 n_classes=hps.n_classes,
                 rep_size=hps.rep_size,
                 mi_units=hps.mi_units,
-                ).to(hps.device)
+                margin=hps.margin).to(hps.device)
 
     optimizer = Adam(sdim.parameters(), lr=hps.lr)
 
-    print('==>  # SDIM parameters: {}.'.format(cal_parameters(sdim)))
+    logger.info('==>  # SDIM parameters: {}.'.format(cal_parameters(sdim)))
 
     if hps.inference:
         inference(sdim, hps)
