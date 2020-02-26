fc30e184cacac331180e550c7948c1f8dd477cbf
王昕 <wangxin@RisingSun.lan>
2020-02-27 01:17:14 +0800

    debug

--- sdim.py
+++ sdim.py
@@ -160,7 +160,7 @@ class SDIM(torch.nn.Module):
     def forward(self, x):
         logits = self.disc_classifier(x)
         rep = self.feature_transformer(logits)
-        log_lik = self.class_conditional(rep)
+        log_lik = self.class_conditional(rep)/self.n_classes
         return log_lik
 
 
