b74ba2c9475c8b7c66181d407209859770737898
王昕 <wangxin@RisingSun.lan>
2020-02-27 23:11:54 +0800

    debug

--- corruption_robustness_eval.py
+++ corruption_robustness_eval.py
@@ -4,6 +4,11 @@ import os
 import sys
 import time
 
+import logging
+import hydra
+from omegaconf import DictConfig
+
+
 import numpy as np
 
 import torch
@@ -11,35 +16,25 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as transforms
 
-from models import ResNeXt, ResNet18
+from models import resnet18, resnet34, resnet50
 from sdim import SDIM
 from utils import cal_parameters, get_dataset, AverageMeter
 
 
-def load_pretrained_model(hps):
-    # Init model, criterion, and optimizer
-    if hps.classifier_name == 'resnext':
-        classifier = ResNeXt(hps.cardinality, hps.depth, hps.n_classes, hps.base_width, hps.widen_factor).to(hps.device)
-    elif hps.classifier_name == 'resnet':
-        classifier = ResNet18(n_classes=hps.n_classes).to(hps.device)
-    else:
-        print('Classifier {} not available.'.format(hps.classifier_name))
+logger = logging.getLogger(__name__)
 
-    print('# Classifier parameters: ', cal_parameters(classifier))
 
-    # if hps.classifier_name == 'resnext':
-    #     save_name = 'ResNeXt{}_{}x{}d.pth'.format(hps.depth, hps.cardinality, hps.base_width)
-    # elif hps.classifier_name == 'resnet':
-    #     save_name = 'ResNet34.pth'
-    #
-    # checkpoint = torch.load(os.path.join(hps.working_dir, save_name), map_location=lambda storage, loc: storage)
-    # print('Load pre-trained checkpoint: {}'.format(save_name))
-    #
-    # classifier.load_state_dict(checkpoint['model_state'])
+def get_model(name='resnet18', n_classes=10):
+    """ get proper model from torchvision models. """
+    model_list = ['resnet18', 'resnet34', 'resnet50']
+    assert name in model_list, '{} not available, choose from {}'.format(name, model_list)
+
+    classifier = eval(name)(n_classes=n_classes)
     return classifier
 
 
 def get_c_dataset(dir='data/CIFAR-10-C'):
+    dir = hydra.utils.to_absolute_path(dir)  # change directory.
     from os import listdir
     files = [file for file in listdir(dir) if file != 'labels.npy']
 
@@ -71,28 +66,24 @@ class CorruptionDataset(Dataset):
         return self.x.shape[0]
 
 
-def inference(sdim, hps):
-    torch.manual_seed(hps.seed)
-    np.random.seed(hps.seed)
 
-    name = 'SDIM_{}_{}.pth'.format(hps.classifier_name, hps.problem)
-    checkpoint_path = os.path.join(hps.log_dir, name)
-
-    sdim.load_state_dict(torch.load(checkpoint_path, map_location=lambda storage, loc: storage)['model_state'])
+def extract_thresholds(sdim, args):
     sdim.eval()
-
     # Get thresholds
-    threshold_list = []
-    for label_id in range(hps.n_classes):
-        # No data augmentation(crop_flip=False) when getting in-distribution thresholds
-        dataset = get_dataset(data_name=hps.problem, train=True, label_id=label_id, crop_flip=False)
-        in_test_loader = DataLoader(dataset=dataset, batch_size=hps.n_batch_test, shuffle=False)
+    threshold_list1 = []
+    threshold_list2 = []
 
-        print('Inference on {}, label_id {}'.format(hps.problem, label_id))
+    data_dir = hydra.utils.to_absolute_path(args.data_dir)
+    for label_id in range(args.get(args.dataset).n_classes):
+        # No data augmentation(crop_flip=False) when getting in-distribution thresholds
+        dataset = get_dataset(data_name=args.dataset, data_dir=data_dir, train=True, label_id=label_id, crop_flip=False)
+        in_test_loader = DataLoader(dataset=dataset, batch_size=args.n_batch_test, shuffle=False)
+
+        logger.info('Extracting thresholds on {}, label_id {}'.format(args.dataset, label_id))
         in_ll_list = []
         for batch_id, (x, y) in enumerate(in_test_loader):
-            x = x.to(hps.device)
-            y = y.to(hps.device)
+            x = x.to(args.device)
+            y = y.to(args.device)
             ll = sdim(x)
 
             correct_idx = ll.argmax(dim=1) == y
@@ -100,52 +91,83 @@ def inference(sdim, hps):
             ll_, y_ = ll[correct_idx], y[correct_idx]  # choose samples are classified correctly
             in_ll_list += list(ll_[:, label_id].detach().cpu().numpy())
 
-        thresh_idx = int(hps.percentile * len(in_ll_list))
-        thresh = sorted(in_ll_list)[thresh_idx]
-        print('threshold_idx/total_size: {}/{}, threshold: {:.3f}'.format(thresh_idx, len(in_ll_list), thresh))
-        threshold_list.append(thresh)  # class mean as threshold
+        thresh_idx = int(0.01 * len(in_ll_list))
+        thresh1 = sorted(in_ll_list)[thresh_idx]
+        thresh_idx = int(0.02 * len(in_ll_list))
+        thresh2 = sorted(in_ll_list)[thresh_idx]
+        threshold_list1.append(thresh1)  # class mean as threshold
+        threshold_list2.append(thresh2)  # class mean as threshold
+        print('1st & 2nd percentile thresholds: {:.3f}, {:.3f}'.format(thresh1, thresh2))
 
-    # Evaluation
-    thresholds = torch.tensor(threshold_list).to(hps.device)
+    thresholds1 = torch.tensor(threshold_list1).to(args.device)
+    thresholds2 = torch.tensor(threshold_list2).to(args.device)
+    return thresholds1, thresholds2
 
-    if hps.no_rejection:
-        hps.percentile=0.0
-        thresholds = thresholds - 1e5   # set thresholds to be very low
 
-#    transform = transforms.Compose([transforms.ToTensor(),
-#                                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+def corruption_eval(sdim, args, thresholds1, thresholds2):
+    sdim.eval()
+    thresholds0 = thresholds1 - 1e5   # set thresholds to be very low, so that no rejection happens.
+
     transform = transforms.ToTensor()
     interval = 10000
 
-    results_dict = dict()
-    for corruption_id, (corruption_type, data, labels) in enumerate(get_c_dataset()):
-        print('==> Corruption type: {}'.format(corruption_type))
+    results_dict0 = dict()
+    results_dict1 = dict()
+    results_dict2 = dict()
+    samples_likelihood_dict = {}
+    for corruption_id, (corruption_type, data, labels) in enumerate(get_c_dataset(args.data_dir)):
+        logger.info('==> Corruption type: {}'.format(corruption_type))
 
         for severity in range(5):
             x_severity = data[severity * interval: (severity + 1) * interval]
             y_severity = labels[severity * interval: (severity + 1) * interval]
 
             dataset = CorruptionDataset(x_severity, y_severity, transform=transform)
-            test_loader = DataLoader(dataset=dataset, batch_size=hps.n_batch_test, shuffle=False, num_workers=4)
+            test_loader = DataLoader(dataset=dataset, batch_size=args.n_batch_test, shuffle=False, num_workers=4)
 
-            n_correct = 0
-            n_false = 0
-            n_reject = 0
+            n_correct0, n_false0, n_reject0 = 0, 0, 0
+            n_correct1, n_false1, n_reject1 = 0, 0, 0
+            n_correct2, n_false2, n_reject2 = 0, 0, 0
 
             for batch_id, (x, target) in enumerate(test_loader):
                 # Note that images are scaled to [-1.0, 1.0]
-                x, target = x.to(hps.device), target.long().to(hps.device)
+                x, target = x.to(args.device), target.long().to(args.device)
 
                 with torch.no_grad():
                     log_lik = sdim(x)
 
                 values, pred = log_lik.max(dim=1)
-                confidence_idx = values >= thresholds[pred]  # the predictions you have confidence in.
-                reject_idx = values < thresholds[pred]       # the ones rejected.
 
-                n_correct += pred[confidence_idx].eq(target[confidence_idx]).sum().item()
-                n_false += (pred[confidence_idx] != target[confidence_idx]).sum().item()
-                n_reject += reject_idx.float().sum().item()
+                if batch_id == 0:
+                    image_name = "sample_{}_severity{}.png".format(corruption_type, severity)
+                    save_image(x[0], image_name, normalize=True)
+                    assert target[0] == pred[0]
+                    samples_likelihood_dict[image_name] = values[0]  # save sample's likelihood.
+
+                def func(thresholds)
+                    confidence_idx = values >= thresholds[pred]  # the predictions you have confidence in.
+                    reject_idx = values < thresholds[pred]       # the ones rejected.
+
+                    n_correct = pred[confidence_idx].eq(target[confidence_idx]).sum().item()
+                    n_false = (pred[confidence_idx] != target[confidence_idx]).sum().item()
+                    n_reject = reject_idx.float().sum().item()
+                    return n_correct, n_false, n_reject
+
+                # Calculate
+                n_c, n_f, n_r = func(thresholds0)
+                n_correct0 += n_c 
+                n_false0 += n_f 
+                n_reject0 += n_r
+
+                n_c, n_f, n_r = func(thresholds1)
+                n_correct0 += n_c 
+                n_false0 += n_f 
+                n_reject0 += n_r
+
+                n_c, n_f, n_r = func(thresholds2)
+                n_correct0 += n_c 
+                n_false0 += n_f 
+                n_reject0 += n_r
 
             n = len(test_loader.dataset)
             acc = n_correct / n
@@ -155,95 +177,43 @@ def inference(sdim, hps):
             acc_remain = acc / (acc + false_rate)
 
             key = '{}_{}'.format(corruption_type, severity + 1)
-            results_dict[key] = {'acc_remain': acc_remain, 'rejection_rate': reject_rate}
+            results_dict0[key] = {'acc_left': n_correct0 / (n_correct0 + n_false0), 'rejection_rate': n_reject0 / n}
+            results_dict1[key] = {'acc_left': n_correct1 / (n_correct1 + n_false1), 'rejection_rate': n_reject1 / n}
+            results_dict2[key] = {'acc_left': n_correct2 / (n_correct2 + n_false2), 'rejection_rate': n_reject2 / n}
 
-            print('Test acc: {:.4f}, false rate: {:.4f}, reject rate: {:.4f}'.format(acc, false_rate, reject_rate))
-            print('acc on remain set: {:.4f}'.format(acc_remain))
-
-    save_path = os.path.join(hps.log_dir, '{}_cifar10-c_per{}_results.pth'.format(hps.classifier_name, int(100*hps.percentile)))
-    torch.save(results_dict, save_path)
+    torch.save(results_dict0, '{}_corruption_percentile0_results.pth'.format(args.classifier_name))
+    torch.save(results_dict1, '{}_corruption_percentile1_results.pth'.format(args.classifier_name))
+    torch.save(results_dict2, '{}_corruption_percentile2_results.pth'.format(args.classifier_name))
 
 
-if __name__ == '__main__':
-    # This enables a ctr-C without triggering errors
-    import signal
+@hydra.main(config_path='configs/corruption_config.yaml')
+def run(args: DictConfig) -> None:
+    cuda_available = torch.cuda.is_available()
+    torch.manual_seed(args.seed)
 
-    signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
+    device = "cuda" if cuda_available and args.device == 'cuda' else "cpu"
 
-    parser = argparse.ArgumentParser(description='PyTorch Implementation of SDIM_logits.')
-    parser.add_argument("--verbose", action='store_true', help="Verbose mode")
-    parser.add_argument("--no_rejection", action="store_true",
-                        help="Used in inference mode with rejection")
-    parser.add_argument("--log_dir", type=str,
-                        default='./logs', help="Location to save logs")
+    n_classes = args.get(args.dataset).n_classes
+    rep_size = args.get(args.dataset).rep_size
+    margin = args.get(args.dataset).margin
+    
+    classifier = get_model(name=args.classifier_name, n_classes=n_classes).to(args.device)
 
-    # Dataset hyperparams:
-    parser.add_argument("--problem", type=str, default='cifar10',
-                        help="Problem cifar10 | svhn")
-    parser.add_argument("--n_classes", type=int,
-                        default=10, help="number of classes of dataset.")
-    parser.add_argument("--data_dir", type=str, default='data',
-                        help="Location of data")
-
-    # Optimization hyperparams:
-    parser.add_argument("--n_batch_train", type=int,
-                        default=128, help="Minibatch size")
-    parser.add_argument("--n_batch_test", type=int,
-                        default=200, help="Minibatch size")
-    parser.add_argument("--optimizer", type=str,
-                        default="adam", help="adam or adamax")
-    parser.add_argument("--lr", type=float, default=0.001,
-                        help="Base learning rate")
-    parser.add_argument("--epochs", type=int, default=20,
-                        help="Total number of training epochs")
-
-    # Inference hyperparams:
-    parser.add_argument("--percentile", type=float, default=0.01,
-                        help="percentile value for inference with rejection.")
-
-    # Architecture for resnext
-    parser.add_argument('--depth', type=int, default=29, help='Model depth.')
-    parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
-    parser.add_argument('--base_width', type=int, default=64, help='Number of channels in each group.')
-    parser.add_argument('--widen_factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
-
-    # sdim hyperparams:
-    parser.add_argument("--image_size", type=int,
-                        default=32, help="Image size")
-    parser.add_argument("--mi_units", type=int,
-                        default=64, help="output size of 1x1 conv network for mutual information estimation")
-    parser.add_argument("--rep_size", type=int,
-                        default=10, help="size of the global representation from encoder")
-    parser.add_argument("--classifier_name", type=str, default='resnet',
-                        help="classifier name: resnet|densenet")
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-
-    # Ablation
-    parser.add_argument("--seed", type=int, default=1234, help="Random seed")
-    hps = parser.parse_args()  # So error if typo
-
-    use_cuda = not hps.no_cuda and torch.cuda.is_available()
-
-    torch.manual_seed(hps.seed)
-
-    hps.device = torch.device("cuda" if use_cuda else "cpu")
-
-    # Create log dir
-    logdir = os.path.abspath(hps.log_dir) + "/"
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-
-    classifier = load_pretrained_model(hps).to(hps.device)
     sdim = SDIM(disc_classifier=classifier,
-                n_classes=hps.n_classes,
-                rep_size=hps.rep_size,
-                mi_units=hps.mi_units,
-                ).to(hps.device)
+                n_classes=n_classes,
+                rep_size=rep_size,
+                mi_units=args.mi_units,
+                margin=margin).to(args.device)
 
-    optimizer = Adam(sdim.parameters(), lr=hps.lr)
+    base_dir = hydra.utils.to_absolute_path('logs/sdim/{}'.format(args.dataset))
+    save_name = 'SDIM_{}.pth'.format(args.classifier_name)
+    sdim.load_state_dict(torch.load(os.path.join(base_dir, save_name), map_location=lambda storage, loc: storage))
 
-    print('==>  # SDIM parameters: {}.'.format(cal_parameters(sdim)))
+    threshold1, threshold2 = extract_thresholds(sdim, args)
+    corruption_eval(sdim, args, thresholds1, thresholds2)
 
-    inference(sdim, hps)
+
+if __name__ == "__main__":
+    run()
+
 
